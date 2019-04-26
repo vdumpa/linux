17c1add74904df628c13a9cd635569eb9760e402
Robin Murphy <robin.murphy@arm.com>
2019-04-26 17:02:24 +0100

    iommu/arm-smmu: Add implementation infrastructure
    Add some nascent infrastructure for handling implementation-specific
    details outside the flow of the architectural code. This will allow us
    to keep mutually-incompatible vendor-specific hooks in their own files
    where the respective interested parties can maintain them with minimal
    chance of conflicts. As somewhat of a template, we'll start with a
    general place to collect the relatively trivial existing quirks.
    
    Signed-off-by: Robin Murphy <robin.murphy@arm.com>

---
+++ drivers/iommu/arm-smmu-impl.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// Miscellaneous Arm SMMU implementation quirks
+// Copyright (C) 2019 Arm Limited
+
+#define pr_fmt(fmt) "arm-smmu: " fmt
+
+#include "arm-smmu.h"
+
+
+int arm_smmu_impl_init(struct arm_smmu_device *smmu)
+{
+	return 0;
+}
